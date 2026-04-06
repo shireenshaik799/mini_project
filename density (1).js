d032938ed3d287407acd22cb08946a9a79a2fcb5
shireenshaik799 <shireenshaik799@gmail.com>
2026-04-06 20:48:14 +0530

    Add files via upload

---
+++ density (1).js
@@ -0,0 +1,26 @@
+var density = {
+
+    AmberFort: 0.8,
+    GatewayOfIndia: 0.9,
+    TajMahal: 1.0,
+    MysorePalace: 0.7,
+    Tirupati: 0.85,
+    JagannathTemple: 0.75,
+    DwarkaTemple: 0.65,
+    BrahmaTemplePushkar: 0.6,
+    Kedarnath: 0.55,
+    Amarnath: 0.5,
+    Badrinath: 0.58,
+    RedFortDelhi: 0.88,
+    GwaliorFort: 0.62,
+    AgraFort: 0.78,
+    JaisalmerFort: 0.57,
+    KangraFort: 0.52,
+    ChittorgarhFort: 0.6,
+    LakshadweepBeach: 0.45,
+    Pondicherry: 0.68,
+    Gokarna: 0.5,
+    VarkalaBeach: 0.48,
+    Goa: 0.82
+
+};
---
+++ locations (1).js
@@ -0,0 +1,26 @@
+var locations = {
+
+    AmberFort: [26.9855, 75.8513],
+    GatewayOfIndia: [18.921984, 72.834654],
+    TajMahal: [27.1751, 78.0421],
+    MysorePalace: [12.3051, 76.6551],
+    Tirupati: [13.6288, 79.4192],
+    JagannathTemple: [19.8047, 85.8189],
+    DwarkaTemple: [22.2442, 68.9685],
+    BrahmaTemplePushkar: [26.4909, 74.5511],
+    Kedarnath: [30.7352, 79.0669],
+    Amarnath: [34.2156, 75.5040],
+    Badrinath: [30.7433, 79.4938],
+    RedFortDelhi: [28.6562, 77.2410],
+    GwaliorFort: [26.2298, 78.1734],
+    AgraFort: [27.1795, 78.0211],
+    JaisalmerFort: [26.9157, 70.9083],
+    KangraFort: [32.0998, 76.2691],
+    ChittorgarhFort: [24.8879, 74.6269],
+    LakshadweepBeach: [10.5667, 72.6417],
+    Pondicherry: [11.9416, 79.8083],
+    Gokarna: [14.5501, 74.3188],
+    VarkalaBeach: [8.7379, 76.7163],
+    Goa: [15.2993, 74.1240]
+
+};
